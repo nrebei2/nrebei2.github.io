602184527bd2d4ab653c2395def87b8aeb3150c0
nrebei2 <rebein002@gmail.com>
2024-01-08 21:47:57 -0500

    Complete work on title card

---
+++ src.js
@@ -0,0 +1,36 @@
+function shuffle(arr) {
+    let curidx = arr.length;
+
+    while (curidx > 0) {
+        ridx = Math.floor(Math.random() * curidx);
+        curidx--;
+
+        [arr[curidx], arr[ridx]] = [
+            arr[ridx], arr[curidx]];
+    }
+
+    return arr;
+}
+
+let techElm = document.querySelector("#techs");
+
+function update_tech() {
+    let techs = ["Python", "OCaml", "C", "C++", "Java", "Coq", "Rust", "HTML", "CSS", "JavaScript", "SQL", "Mathematica", "Unity", "ROS"];
+    shuffle(techs);
+
+    let curidx = 0;
+
+    let update = () => {
+        if (curidx == techs.length) {
+            curidx = 0;
+            shuffle(techs);
+        }
+        techElm.textContent = techs[curidx];
+        curidx++;
+    }
+    return update;
+}
+
+let update = update_tech();
+update();
+techElm.addEventListener("animationiteration", update);
